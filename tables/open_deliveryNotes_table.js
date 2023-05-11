c6db11904ccbe0cb3e6dcc54147d35efe2c02a2f
Balazs Perak <perakbalazs1986@gmail.com>
2023-05-11 17:24:00 +0200

    frissített

--- tables/open_deliveryNotes_table.js
+++ tables/open_deliveryNotes_table.js
@@ -6,30 +6,26 @@ fetch('http://localhost:3001/delivery_notes')
         // console.log(datas);
         datas.map(data => {
             // console.log(data);
-            open_delivery_tbody.append(trFunction(data.serial_no, data.partner_id, data.owner_id,data.carrier_id,data.created_at,data.status,data.id))
+            if (data.status == "Nyitott") {
+               trFunction(data.serial_no, data.partner_id, data.owner_id, data.carrier_id, data.created_at, data.status, data.id)
+            }
         });
         jumpToRow();
     })
 
-function trFunction(serial_no, partner_id,owner_id,carrier_id,created_at,status,id) {
-
-    //csak teszt itt sokat kell még változtatni
-    let tr = document.createElement('tr');
-    tr.innerHTML =
-        `
-    <td class="vat_number">${serial_no}</td>
-    <td class="name" style="font-weight: bold;">${serial_no}</td>
-    <td class="bank_account">${serial_no}</td>
-    <td class="memo">${serial_no}</td>
-    <td class="memo">${serial_no}</td>
-    <td class="memo">${serial_no}</td>
-    <td class="deliveryNotes_id">${id}</td>
-    `
-    tr.ondblclick = function (event) {
-        myEditFunction(event, id);
-    }
-    tr.setAttribute( "id", id);
-    return tr;
+function trFunction(serial_no, partner_id, owner_id, carrier_id, created_at, status, id) {
+    
+    fetch(`http://localhost:3001/partners/${partner_id}`)
+    .then(response => response.json())
+    .then(datas => {
+        datas.map(data => {
+             //console.log(data.name);
+            GetOwnerName(serial_no,data.name,owner_id,carrier_id,created_at,status, id)
+        })
+       
+    })
+    
+    
 }
 
 function myEditFunction(event, id) {
@@ -43,16 +39,67 @@ function myEditFunction(event, id) {
 }
 
 // ha volt új, vagy szerkesztett sor, odaugrik
-function jumpToRow(){
+function jumpToRow() {
     let back_id = localStorage.getItem('back_id');
-    if (back_id){
-        console.log( 'back_id: ', back_id)
-        let row = document.getElementById( back_id);
-        if (row){
-            row.scrollIntoView( true);
+    if (back_id) {
+        console.log('back_id: ', back_id)
+        let row = document.getElementById(back_id);
+        if (row) {
+            row.scrollIntoView(true);
         }
         localStorage.clear();
     }
 }
+
+function  GetOwnerName(serial_no,partner_name,owner_id,carrier_id,created_at,status, id) {
+
+    console.log();
+    fetch(`http://localhost:3001/owners/${owner_id}`)
+    .then(response => response.json())
+    .then(datas => {
+        datas.map(data => {
+             //console.log(data.name);
+            GetCarrierName(serial_no,partner_name,data.name,carrier_id,created_at,status, id)
+        })
+       
+    })
+}
+
+function  GetCarrierName(serial_no,partner_name,owner_name,carrier_id,created_at,status, id) {
+
+    console.log();
+    fetch(`http://localhost:3001/carriers/${carrier_id}`)
+    .then(response => response.json())
+    .then(datas => {
+        datas.map(data => {
+             //console.log(data.name);
+            Table(serial_no,partner_name,owner_name,data.name,created_at,status, id)
+        })
+       
+    })
+}
+
+function Table(serial_no,partner_name,owner_name,carrier_name,created_at,status, id) {
+
+    //console.log(serial_no,partner_name,owner_name,carrier_name,created_at,status, id);
+    let tr = document.createElement('tr');
+    tr.innerHTML =
+        `
+    <td class="serial_no"style="font-weight: bold;">${serial_no}</td>
+    <td class="partner_name" >${partner_name}</td>
+    <td class="owner_name">${owner_name}</td>
+    <td class="carrier_name">${carrier_name}</td>
+    <td class="creation_date">${created_at}</td>
+    <td class="status">${status}</td>
+    <td class="deliveryNotes_id">${id}</td>
+    `
+    tr.ondblclick = function (event) {
+        myEditFunction(event, id);
+    }
+    tr.setAttribute("id", id);
+    open_delivery_tbody.append(tr);
+    
+}
+
 
 
