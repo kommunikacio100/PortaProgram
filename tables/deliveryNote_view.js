c78d23e32e6c6890cc69ba597e87518aa8ebe9e2
Balazs Perak <perakbalazs1986@gmail.com>
2023-05-13 13:30:25 +0200

    nem tudok beleilleszteni értéket

--- tables/deliveryNote_view.js
+++ tables/deliveryNote_view.js
@@ -1,1 +1,116 @@
-window.print();
+//window.print();
+
+const cached_deliveryView_id = localStorage.getItem('deliveryNote_id');
+
+console.log(cached_deliveryView_id);
+
+if (cached_deliveryView_id) {
+    fetch('http://localhost:3001/delivery_notes/' + cached_deliveryView_id)
+        .then(response => response.json())
+        .then(datas => {
+            
+            datas.map(data => {
+
+                console.log(data.owner_id);
+                
+                if(data.owner_id!=null){
+                fetch('http://localhost:3001/owners/' + data.owner_id)
+                    .then(response => response.json())
+                    .then(datas => {
+                        
+                        datas.map(data => {
+                            console.log(data.name);
+                            document.getElementById('input_owner_name').value = data.name;
+                            
+                        })
+                    })
+                }
+                if(data.owner_address_id!=null){
+                fetch('http://localhost:3001/addresses/' + data.owner_address_id)
+                    .then(response => response.json())
+                    .then(datas => {
+                        
+                        datas.map(data => {
+                            
+                            document.getElementById('input_owner_address').value = data.zip_code + " " + data.city + " " + data.street_name + " " + data.street_type + " " + data.street_number;
+                            document.getElementById('input_location_address').value = data.zip_code + " " + data.city + " " + data.street_name + " " + data.street_type + " " + data.street_number;
+
+                        })
+                    })
+                }
+                if(data.partner_id!=null){
+                fetch('http://localhost:3001/partners/' + data.partner_id)
+                    .then(response => response.json())
+                    .then(datas => {
+                       
+                        datas.map(data => {
+                            
+                            document.getElementById('input_partner_name').value = data.name + " " + data.vat_number;
+                            
+                        })
+                    })
+                }
+                if(data.partner_address_id!=null){
+                fetch('http://localhost:3001/addresses/' + data.partner_address_id)
+                    .then(response => response.json())
+                    .then(datas => {
+                        
+                        datas.map(data => {
+                            
+                            document.getElementById('input_partner_address').value = data.zip_code + " " + data.city + " " + data.street_name + " " + data.street_type + " " + data.street_number;
+                            document.getElementById('input_unloadlocation_address').value = data.zip_code + " " + data.city + " " + data.street_name + " " + data.street_type + " " + data.street_number;
+
+                        })
+                    })
+                }
+                if(data.carrier_id!=null){
+                fetch('http://localhost:3001/carriers/' + data.carrier_id)
+                    .then(response => response.json())
+                    .then(datas => {
+                        
+                        datas.map(data => {
+                            
+                            document.getElementById('input_carrier_name').value = data.name;
+                            
+                        })
+                    })
+                }
+                if(data.carrier_address_id!=null){
+                fetch('http://localhost:3001/addresses/' + data.carrier_address_id)
+                    .then(response => response.json())
+                    .then(datas => {
+                       
+                        datas.map(data => {
+                            
+                            document.getElementById('input_carrier_address').value = data.zip_code + " " + data.city + " " + data.street_name + " " + data.street_type + " " + data.street_number;
+
+                        })
+                    })
+                }
+
+                if(data.movement_id!=null){
+                    let move=data.movement_id;
+                fetch('http://localhost:3001/movements')
+                    .then(response => response.json())
+                    .then(datas => {
+                       
+                        datas.map(data => {
+                            
+                            if(data.id==move){
+                                document.getElementById('input_movement').value = data.name;
+                            }
+
+                        })
+                    })
+                }
+
+
+                document.getElementById('input_status').value = data.status;
+                //document.getElementById('input_deliveryNote_id').value = data.id;
+                document.getElementById('serial_no').value = data.serial_no;
+
+
+            })
+        })
+    localStorage.clear();
+}
