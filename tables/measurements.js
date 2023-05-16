77a2a1165f6031fb6525060ff1ee2a92ab84b271
Balazs Perak <perakbalazs1986@gmail.com>
2023-05-16 17:43:40 +0200

    update

--- tables/measurements.js
+++ tables/measurements.js
@@ -1,156 +1,71 @@
-
 import { authToken, serverUrl, requestOptions } from './requestOptions.js';
 
-const measurement_id = localStorage.getItem('measurement_id');
-console.log(measurement_id);
+let measurements_tbody = document.getElementById('tbody');
 
-
-fetch( serverUrl + `/measurements/${measurement_id}`, requestOptions)
+fetch(  serverUrl+ '/measurements', requestOptions)
     .then(response => response.json())
     .then(datas => {
         // console.log(datas);
         datas.map(data => {
             // console.log(data);
-            //document.getElementById("input_plate_number").value=data.vehicle_id;
-
-            fetch( serverUrl + `/vehicles/${data.vehicle_id}`, requestOptions)
-                .then(response => response.json())
-                .then(datas => {
-                    //console.log(datas);
-                    datas.map(data => {
-                        //console.log(data);
-                        document.getElementById("input_plate_number").value = data.plate_number1;
-                    });
-                })
-
-            fetch( serverUrl + `/products/${data.product_id}`, requestOptions)
-                .then(response => response.json())
-                .then(datas => {
-                    //console.log(datas);
-                    datas.map(data => {
-                        //console.log(data);
-                        document.getElementById("product").value = data.name + ', ' + data.item_number;
-                    });
-                })
-
-            document.getElementById("weight_inbound").value = data.first_weight;
-
-
-            //ezt javítnai kell
-            console.log(data.first_time);
-            document.getElementById("weight_time_inbound").value = data.first_time.substring(0, data.first_time.length-5);;
-
-
+            
+            measurements_tbody.append(trFunction(data.vehicle_id, data.product_id, data.first_weight, data.second_weight, data.net_weight,data.id))
+            
         });
-
+       // jumpToRow();
     })
 
-fetch( serverUrl + '/products', requestOptions)
+function trFunction(vehicle_id, product_id,first_weight, second_weight, net_weight,id) {
+    
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td class="vehicle_id" style="font-weight: bold;"></td>
+    <td class="product_id"></td>
+    <td class="first_weight">${first_weight}</td>
+    <td class="second_weight">${second_weight}</td>
+    <td class="net_weight">${net_weight}</td>
+    <td class="id">${id}</td>
+        `
+        
+    if(vehicle_id!=null){
+    fetch( serverUrl+ `/vehicles/${vehicle_id}`,requestOptions)
     .then(response => response.json())
     .then(datas => {
-        //console.log(datas);
-        let option = document.createElement('option');
-        option.innerText = 'válasszon terméket!';
-        product.append(option);
         datas.map(data => {
-            //console.log(data);
-            let option = document.createElement('option');
-            option.innerText = data.name + ', ' + data.item_number;
-            option.id = data.id;
-            product.append(option);
-        });
+             //console.log(data.name);
+             tr.querySelector('.vehicle_id').textContent = data.plate_number1;
+        })
+       
     })
-
-scaleWeightBtn = document.getElementById("scaleWeightBtn");
-emptyWeightBtn = document.getElementById("emptyWeightBtn");
-
-weight_outbound = document.getElementById("weight_outbound");
-weight_time_outbound = document.getElementById("weight_time_outbound");
-
-scaleWeightBtn.addEventListener("click", (event) => {
-    fetch( serverUrl + '/scaleWeight', requestOptions)
-        .then(response => response.json())
-        .then( json => {
-            console.log('JSON', json);
-            weight_outbound.value = json[0].scaleWeight;
-            let dateTime = new Date().toISOString();
-            dateTime = dateTime.substring(0, dateTime.length - 2);
-            console.log(dateTime);
-            weight_time_outbound.value = dateTime;
+    }
+    
+    if(product_id!=null){
+    fetch( serverUrl+ `/products/${product_id}`)
+    .then(response => response.json())
+    .then(datas => {
+        datas.map(data => {
+             //console.log(data.name);
+             tr.querySelector('.product_id').textContent = data.name+" "+data.item_number;
         })
-})
-
-const submit_button_visszameres = document.getElementById("submit_button");
-
-submit_button_visszameres.addEventListener("click", (event) => {
-    event.preventDefault();
-    create_and_update_measure();
-})
-
-function create_and_update_measure() {
-
-    fetch( serverUrl + `/measurements/${measurement_id}`, requestOptions)
-        .then(response => response.json())
-        .then(datas => {
-            // console.log(datas);
-            datas.map(data => {
-
-                var id = measurement_id;
-                var delivery_note_id=data.delivery_note_id;
-                console.log(delivery_note_id);
-                var vehicle_id=data.vehicle_id;
-                var product_id=data.product_id;
-                var first_weight=data.first_weight;
-                var second_weight=document.getElementById("weight_outbound").value;
-                var first_time=data.first_time;
-                var second_time= document.getElementById("weight_time_outbound").value;
-                var net_weight="";
-                var first_type=data.first_type;
-                var second_type="M";
-
-                //var delivery_note_id = cached_delivery;
-                //var vehicle_id = document.getElementById('input_plate_number').value;
-                //var product_id = document.getElementById('product');
-                //product_id=product_id[product_id.selectedIndex].id;
-                //console.log(product_id);
-                //var first_weight = document.getElementById('weight_inbound').value;
-                //var first_time = document.getElementById('weight_time').value;
-                
-
-                let data_to_send = {
-                    "id": id,
-                    "delivery_note_id": delivery_note_id,
-                    "vehicle_id": vehicle_id,
-                    "second_weight": second_weight,
-                    "product_id": product_id,
-                    "first_weight": first_weight,
-                    "first_time": first_time,
-                    "second_time": second_time,
-                    "net_weight": net_weight,
-                    "first_type": first_type,
-                    "second_type": second_type
-
-
-                }
-
-                let amethod = "POST"
-                if (id === '') amethod = "POST"
-                else amethod = "PUT";
-
-                console.log('data_to_send ', data_to_send);
-                fetch( serverUrl + "/measurements", {
-                    method: amethod,
-                    body: JSON.stringify(data_to_send),
-                    headers: {
-                        "Content-type": "application/json",
-                        'Authorization': `Bearer ${authToken}` // az auth token hozzáadása az Authorization header-hez
-                    }
-                }).then(result => {
-                    console.log(result)
-                    //localStorage.setItem('back_id', id);
-                    //redirectToOwnerTable();
-                });
-
-            })
+       
+    })
+    }
+    /*
+    if(carrier_id!=null){
+    fetch( serverUrl+ `/carriers/${carrier_id}`)
+    .then(response => response.json())
+    .then(datas => {
+        datas.map(data => {
+             //console.log(data.name);
+             tr.querySelector('.carrier_name').textContent = data.name;
+            
         })
+       
+    })
+    }
+        
+    */
+    return tr;
+    
+    
 }
